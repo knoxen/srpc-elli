45e065b45b0f3c96cda56271009de95f14b733b1
dingo sky <paul@dingosky.com>
2017-11-04 14:42:56 -0700

    Dummy config

--- config/config.exs
+++ config/config.exs
@@ -1,28 +1,9 @@
 use Mix.Config
 
-# config :logger,
-#   level: :debug,
-#   utc_log: true,
-#   backends: [:console,
-#              {LoggerFileBackend, :debug},
-#              {LoggerFileBackend, :info},
-#              {LoggerFileBackend, :warn},
-#              {LoggerFileBackend, :error}],
-#   compile_time_purge_level: :info
-
-# config :logger, :debug,
-#   path: "/tmp/log/srpc_elli_debug.log",
-#   level: :debug
-
-# config :logger, :info,
-#   path: "/tmp/log/srpc_elli_info.log",
-#   level: :info
-
-# config :logger, :warn,
-#   path: "/tmp/log/srpc_elli_warn.log",
-#   level: :warn
-
-# config :logger, :error,
-#   path: "/tmp/log/srpc_elli_error.log",
-#   level: :error
-
+config :srpc_elli,
+  ## Required
+  port: 4444,
+  srpc_handler: Dummy.SrpcHandler,
+  elli_handlers: [
+    {Dummy.ElliHandler, []}
+  ]
