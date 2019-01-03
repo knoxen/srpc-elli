b164412822093a0f1674d73793122df1807bf7bf
dingo sky <paul@dingosky.com>
2019-01-03 15:51:44 -0700

    Ignore local dir

--- lib/srpc_elli/supervisor.ex
+++ lib/srpc_elli/supervisor.ex
@@ -3,28 +3,36 @@ defmodule SrpcElli.Supervisor do
 
   use Supervisor
 
+  def child_spec([]) do
+    %{id: __MODULE__, start: {__MODULE__, :start_link, []}, type: :supervisor}
+  end
+  
   def start_link() do
     Supervisor.start_link(__MODULE__, [], name: __MODULE__)
   end
 
   def init([]) do
-    # Ensure each module in elli_stack is actually an elli_handler
-    elli_stack = Application.get_env :srpc_elli, :stack
-    elli_stack
-    |> Keyword.keys
-    |> Enum.each(fn(mod) -> mod |> validate_elli_handler end)
+    :ok =
+      srpc(:file)
+      |> File.read!
+      |> :srpc_lib.init
 
-    elli_port = Application.get_env :srpc_elli, :port
-    elli_config = [{:mods, elli_stack}]
+    Application.put_env(:srpc_srv, :srpc_handler, srpc(:handler))
+    
+    elli_config = [{:mods, elli_stack()}]
     elli_opts = [
       {:callback, :elli_middleware},
       {:callback_args, elli_config},
-      {:port, elli_port},
+      {:port, elli(:port)},
+      {:min_acceptors, 10},
       {:name, {:local, :elli}}
     ]
 
     children = [
-      Supervisor.Spec.worker(:elli, [elli_opts])
+      %{
+        id: Elli,
+        start: {:elli, :start_link, [elli_opts]}
+      }
     ]
 
     Supervisor.init(children, strategy: :one_for_one)
@@ -32,12 +40,37 @@ defmodule SrpcElli.Supervisor do
 
   def init(_args), do: throw(:invalid_args)
 
+  defp param(mod, key) do
+    value =
+      :srpc_elli
+      |> Application.get_env(mod)
+      |> Keyword.get(key)
+
+    unless value do
+      raise SrpcElli.Error, message: "SrpcElli: Required #{mod} configuration for #{key} missing"
+    end
+    value
+  end
+
+  defp srpc(param), do: param(:srpc, param)
+  
+  defp elli(param), do: param(:elli, param)
+
+  defp elli_stack do
+    stack = elli(:stack)
+    # Ensure each module in elli_stack is actually an elli_handler
+    stack
+    |> Keyword.keys
+    |> Enum.each(fn(mod) -> mod |> validate_elli_handler end)
+
+    stack
+  end
+
   defp validate_elli_handler(mod) do
     funs = mod.__info__(:functions)
     unless funs |> Enum.member?({:handle, 2}) and funs |> Enum.member?({:handle_event, 3}) do
       throw "Invalid module in elli_stack: #{mod} does not provide elli_handler behaviour"
     end
-      
   end
-
+  
 end
--- local/srpc_lib/ebin/srpc_lib.app
+++
@@ -1,9 +0,0 @@
-{application,srpc_lib,
-             [{description,"Knoxen SRPC Lib"},
-              {vsn,"0.15.1+build.145.ref36c6c87"},
-              {registered,[]},
-              {modules,[srpc_encryptor,srpc_key_agreement,srpc_lib,
-                        srpc_lib_key_agreement,srpc_registration,srpc_sec,
-                        srpc_user_key_agreement,srpc_util]},
-              {applications,[kernel,stdlib,crypto]},
-              {env,[]}]}.
